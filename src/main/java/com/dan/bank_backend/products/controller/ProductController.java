d2affb6f8f432832ef701d311432e3eb99c59eb5
danjrbravo <danjrbrav0@gmail.com>
2026-09-17 22:15:54 -0500

    feat(product): add controller

---
+++ src/main/java/com/dan/bank_backend/products/controller/ProductController.java
@@ -0,0 +1,55 @@
+package com.dan.bank_backend.products.controller;
+
+import com.dan.bank_backend.products.dtos.CreateProductRequestDTO;
+import com.dan.bank_backend.products.dtos.ProductDTO;
+import com.dan.bank_backend.products.service.imp.ProductServiceImp;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+    private final ProductServiceImp productService;
+
+    @GetMapping
+    public ResponseEntity<List<ProductDTO>> getAll(){
+        return ResponseEntity.ok(productService.getAll());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> getById(@PathVariable Long id){
+        return ResponseEntity.ok(productService.findProductById(id));
+    }
+    @PostMapping
+    public ResponseEntity<ProductDTO> createProduct(@Valid @RequestBody CreateProductRequestDTO productDTO){
+        ProductDTO newProduct = productService.saveProduct(productDTO);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(newProduct);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProductById(@PathVariable Long id){
+        productService.deleteProductBy(id);
+        return ResponseEntity.noContent().build();
+    }
+    @PutMapping("/{id}/activate")
+    public ResponseEntity<Void> activateProduct(@PathVariable Long id){
+        productService.activateProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+    @PutMapping("/{id}/deactivate")
+    public ResponseEntity<Void> disableProduct(@PathVariable Long id){
+        productService.disableProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+    @PutMapping("/{id}/cancel")
+    public ResponseEntity<Void> cancelProduct(@PathVariable Long id){
+        productService.cancelProduct(id);
+        return ResponseEntity.noContent().build();  
+    }
+}
---
+++ src/main/java/com/dan/bank_backend/products/controller/advice/ProductControllerAdvice.java
@@ -0,0 +1,15 @@
+package com.dan.bank_backend.products.controller.advice;
+
+import com.dan.bank_backend.products.exceptions.ProductNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ProductControllerAdvice {
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<String> handleProductNotFound(ProductNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+}
